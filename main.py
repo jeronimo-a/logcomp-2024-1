0ebad5aac10fcb06e18207fe8c31848d7e5380b7
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-02-19 16:46:23 -0300

    Esqueleto das classes feitos

--- main.py
+++ main.py
@@ -1,45 +1,32 @@
 import sys
 
-alphabet = "+-1234567890 "
+class Token:
 
-def soma():
+    def __init__(self, token_type: str, value: str):
+        self.type   : str = token_type  # tipo do Token, tal como inteiro, sinal de mais, etc.
+        self.value  : str = value       # valor do Token, para inteiros, o número em si
 
-    try: input_string = sys.argv[1]     # extrai o argumento
-    except IndexError: raise Exception  # se não houver argumento, dá erro
 
-    # filtra os tokens desconhecidos
-    for token in input_string:
-        if token not in alphabet:
-            raise Exception     # token desconhecido
+class Tokenizer:
 
-    input_string = input_string.replace("+", " + ") # insere espaços no símbolo da soma, para remover casos tipo "1+1"
-    input_string = input_string.replace("-", " - ") # insere espaços no símbolo da subtração, para remover casos tipo "1-1"
-    elements = input_string.split(" ")              # divide a string em uma lista de elementos a partir dos espaços
-    elements = [i for i in elements if i != ""]     # remove as strings vazias
-
-    # a entrada deve ter um número ímpar de elementos
-    # se o número for par, adiciona um elemento inválido
-    if len(elements) % 2 == 0:
-        elements.append("-")
-
-    # verifica se o primeiro elemento é um número
-    if not elements[0].isdigit():
-        raise Exception
+    def __init__(self, source: str):
+        self.source     : str   = source    # o código fonte
+        self.position   : int   = 0         # posição atual que está sendo separada
+        self.next       : Token = None      # o último token separado
     
-    soma = int(elements[0])                     # incializa o acumulável como o primeiro elemento
-    for i in range(len(elements) - 1, 1, -2):   # itera sobre os elementos pares, que devem ser números
-
-        signal = elements[i - 1]
-        number = elements[i]
-        
-        # verifica a validade do sinal e do número
-        if signal.isdigit() or not number.isdigit():
-            raise Exception
-        
-        soma += int(signal + number)
-
-    return soma
+    def select_next(self):
+        pass
 
 
-if __name__ == "__main__":
-    print(soma())
+class Parser:
+
+    def __init__(self):
+        self.tokenizer  : Tokenizer = None  # instância da classe Tokenizer que irá ler o código fonte e alimentar o Parser
+
+    @staticmethod
+    def parseExpression():
+        pass
+
+    @staticmethod
+    def run(source: str):
+        pass
