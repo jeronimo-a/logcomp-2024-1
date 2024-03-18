79eae57c4afff24ba43fe7f9652e8408c9ff547b
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-03-18 17:19:59 -0300

    Tokenizer atualizado

--- langtokenizer.py
+++ langtokenizer.py
@@ -2,6 +2,11 @@ from langtoken import Token
 
 class Tokenizer:
 
+    allowed_chars  = "abcdefghijklmnopqrstuvwxyz"
+    allowed_chars += allowed_chars.upper()
+    allowed_chars += "0123456789_"
+    reserved_words = {"print": "PRINT"}
+
     def __init__(self, source: str):
         self.source     : str   = source    # o código fonte
         self.position   : int   = 0         # posição atual que está sendo separada
@@ -32,6 +37,21 @@ class Tokenizer:
             token_value     += self.source[self.position]
             self.position   += 1
             if self.over(): break
+
+        # se o caractere atual não for numérico nem + - / * () ou \n
+        if not self.over():
+            if self.source[self.position] in Tokenizer.allowed_chars and not self.source[self.position].isnumeric():
+                identifier = ""
+                while self.source[self.position] in Tokenizer.allowed_chars:
+                    identifier    += self.source[self.position]
+                    self.position += 1
+                    if self.over(): break
+                if identifier in Tokenizer.reserved_words.keys():
+                    self.next = Token(Tokenizer.reserved_words[identifier], identifier)
+                    return
+                else:
+                    self.next = Token("IDENT", identifier)
+                    return
         
         # se o token já tiver sido definido como inteiro, cria o token e termina a função
         if token_type == "INT":
@@ -71,6 +91,12 @@ class Tokenizer:
         # se for fechamento de parênteses, cria o token e termina a função
         if self.source[self.position] == ")":
             self.next       = Token("CLOSEPAR", ")")
+            self.position  += 1
+            return
+        
+        # se for assignment, cria o token e termina a função
+        if self.source[self.position] == "=":
+            self.next       = Token("ASSIGN", "=")
             self.position  += 1
             return
         
