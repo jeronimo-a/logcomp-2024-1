88683b1a9f4d29dfe401b70ee554485f7b5e47ca
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-04-08 19:47:55 -0300

    Atualização da classe Token

--- langtoken.py
+++ langtoken.py
@@ -1,11 +1,11 @@
+import utils
+
 class Token:
 
-    # gera a lista de tipos de tokens válidos definida em tokentypes.txt
-    with open("tokentypes.txt") as file:
-        types = file.read().split("\n")
+    # gera as listas de tipos de tokens existentes e palavras reservadas
+    types, reserved_words = utils.make_lists("tokentypes.txt")
 
     def __init__(self, token_type: str, value: str):
         if token_type not in Token.types: raise Exception("Tipo de Token inválido: %s" % token_type)
         self.type   : str = token_type  # tipo do Token, tal como inteiro, sinal de mais, etc.
         self.value  : str = value       # valor do Token, para inteiros, o número em si
-    
---
+++ utils.py
@@ -0,0 +1,19 @@
+def remove_all_empty_str(iterable):
+    result = iterable.copy()
+    while True:
+        try: result.remove('')
+        except ValueError: break
+    return result
+
+
+def make_lists(filename):
+
+    with open(filename) as file:
+        contents = file.read().split("#")
+        
+    types = contents[1].split("\n")[1:]
+    words = contents[2].split("\n")[1:]
+    types = remove_all_empty_str(types)
+    words = remove_all_empty_str(words)
+
+    return types, words
