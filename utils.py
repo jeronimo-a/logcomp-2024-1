a519588daf7cbffb583c4766c8339a777d515409
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-04-08 20:04:24 -0300

    Mais verbose

--- utils.py
+++ utils.py
@@ -22,6 +22,8 @@ def read_token_types_file(filename):
             word = word.replace("\t", " ")
             values = word.split(" ")
             values = remove_all_empty_str(values)
+            if values[1] not in types:
+                raise Exception('Tipo "%s" para a palavra reservada "%s" não existe' % (values[1], values[0]))
             words_dict[values[0]] = values[1]
 
         return types, words_dict
