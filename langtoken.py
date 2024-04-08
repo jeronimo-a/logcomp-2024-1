987bc2d73c507c8e6589a3a0bfefd9179f67163e
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-04-08 19:34:59 -0300

    Atualização da classe Token

--- langtoken.py
+++ langtoken.py
@@ -1,6 +1,11 @@
 class Token:
 
+    # gera a lista de tipos de tokens válidos definida em tokentypes.txt
+    with open("tokentypes.txt") as file:
+        types = file.read().split("\n")
+
     def __init__(self, token_type: str, value: str):
+        if token_type not in Token.types: raise Exception("Tipo de Token inválido: %s" % token_type)
         self.type   : str = token_type  # tipo do Token, tal como inteiro, sinal de mais, etc.
         self.value  : str = value       # valor do Token, para inteiros, o número em si
     
