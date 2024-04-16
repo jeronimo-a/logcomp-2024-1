61866009ee2f164b67a7d367c96b1f16b2080a4e
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-04-15 21:55:14 -0300

    PrePro não remove linhas vazias de string, já que é pra aceitar string multiline

--- langprepro.py
+++ langprepro.py
@@ -16,12 +16,20 @@ class PrePro:
                 break
             text = text[:start] + text[end:]
 
-        # remove linhas vazias
-        new_text = text.replace("\n\n", "\n")
-        while new_text != text:
-            text = new_text
-            new_text = text.replace("\n\n", "\n")
+        # remove linhas vazias fora de strings
+        in_str = False
+        indexes = list()
+        for i in range(len(text)):
+            if text[i] == '"':
+                in_str = not in_str
+                continue
+            if not in_str and text[i:i+2] == "\n\n":
+                indexes.append(i)
+        indexes.reverse()
+        for i in indexes:
+            text = text[:i] + text[i+1:]
 
+        # acrescenta um newline no final, se não haver
         if text[-1] != "\n":
             text += "\n"
 
