07346b8820d66d641e2951ca332f526c9574bc60
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-04-29 16:56:50 -0300

    Roteiro 8: symbol table atualizada

--- langsymboltable.py
+++ langsymboltable.py
@@ -1,22 +1,21 @@
 class SymbolTable:
 
+    shift = 4
+
     def __init__(self):
-        self.value_table = dict()
-        self.type_table = dict()
+        self.table = dict()
 
     def get(self, key: str):
-        return self.value_table[key], self.type_table[key]
+        return self.table[key]
     
-    def set(self, key: str, value, type: str):
-        _ = self.value_table[key]
-        _ = self.type_table[key]
-        self.value_table[key] = value
-        self.type_table[key] = type
+    def set(self, key: str, type: str, value):
+        _ = self.table[key]
+        self.table[key] = (type, value, self.table[key][2])
 
     def init(self, key: str):
         try: 
             self.get(key)
             raise ValueError('Variável "%s" já foi declarada.' % key)
         except KeyError: pass
-        self.value_table[key] = None
-        self.type_table[key] = None
+        self.table[key] = (None, None, SymbolTable.shift)
+        SymbolTable.shift += 4
